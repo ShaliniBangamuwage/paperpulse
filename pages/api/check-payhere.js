e3d20ec0a4ed93d8b372468e9743471833851f84
Shalini Madhuka <madukadilhari63@gmail.com>
2026-05-26 11:59:09 +0530

    remove check-payhere api

--- pages/api/check-payhere.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  res.json({
-    exists: !!process.env.PAYHERE_MERCHANT_SECRET,
-    firstChars:
-      process.env.PAYHERE_MERCHANT_SECRET?.slice(0, 5),
-  });
-}
